87c2a0b0392493889ffe76607745a8a73088c2a1
Donielle Berg <donielle.berg@gmail.com>
2019-02-26 10:22:59 -0800

    Set up DSR with create-react-app and add IconPath & Redux to index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { IconSettings } from '@salesforce/design-system-react';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <IconSettings iconPath="icons">
+      <App />
+    </IconSettings>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
