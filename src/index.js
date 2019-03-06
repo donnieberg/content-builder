dea27e574f8974d52796b403e92e4a33872b8114
Donielle Berg <donielle.berg@gmail.com>
2019-03-06 15:00:10 -0800

    Add redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import rootReducer from './reducers'
 import { IconSettings } from '@salesforce/design-system-react';
 
-ReactDOM.render(
-  <IconSettings iconPath="icons">
-    <App />
-  </IconSettings>,
-  document.getElementById('root')
-);
+const store = createStore(rootReducer)
 
+render(
+  <Provider store={store}>
+    <IconSettings iconPath="icons">
+      <App />
+    </IconSettings>
+  </Provider>,
+  document.getElementById('root')
+)
