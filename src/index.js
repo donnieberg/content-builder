ff404ddeb1e21c0fb1170c3bba537ee410933673
Lee White <leewhite@salesforce.com>
2019-03-28 11:24:11 -0700

    undid adding a11y-components

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-  
+
 import App from './App';
-import rootReducer from './redux/reducers';
+// import rootReducer from './redux/reducers';
 import { IconSettings } from '@salesforce/design-system-react';
 
 render(
