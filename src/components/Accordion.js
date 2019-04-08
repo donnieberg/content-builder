da2a83efda97e0528db02856647228dcdcab1f35
Lee White <leewhite@salesforce.com>
2019-04-08 14:30:42 -0700

    fixed accordion expand/collapse

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -11,28 +11,28 @@ class Accordion extends Component {
     this.state = {
       expandedPanels: {},
     };
+    this.togglePanel = this.togglePanel.bind(this)
   }
 
-  // toggle panel doesnt work lol
-  togglePanel(event, data) {
+  togglePanel(event, id) {
     this.setState((state) => ({
       ...state,
       expandedPanels: {
         ...state.expandedPanels,
-        [data.id]: !state.expandedPanels[data.id],
+        [id]: !state.expandedPanels[id],
       },
     }));
   }
 
   render() {
     return (
-      <AccordionWrapper id="base-example-accordion" className={`white-bkgd ${this.props.className}`}>
+      <AccordionWrapper id={this.props.id} className={`white-bkgd ${this.props.className}`}>
         {this.props.children.map((item, i) => (
           <AccordionPanel
-            expanded={!!this.state.expandedPanels[item.id]}
-            id={item.id}
-            key={item.id}
-            onTogglePanel={(event) => this.togglePanel(event, item)}
+            expanded={!!this.state.expandedPanels[`${this.props.id}-${i}`]}
+            id={`${this.props.id}-${i}`}
+            key={`${this.props.id}-${i}`}
+            onTogglePanel={(event) => this.togglePanel(event, `${this.props.id}-${i}`)}
             summary={item.label}
           >
             {item.content}
