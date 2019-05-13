87e58faeec3a24c8a5d3cdd71a3f320e4321e965
Lee White <leewhite@salesforce.com>
2019-05-13 13:55:49 -0700

    removed listbox-y stuff from sidepanel

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,29 +2,8 @@ import React, { Component } from 'react';
 import { Button, Icon } from '@salesforce/design-system-react';
 
 import { ALL_COMPONENTS } from '../redux/constants';
-import { getNewIndex } from '../helpers';
 
 class Sidebar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { currIndex: 0 };
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    let currComponent = ALL_COMPONENTS[this.state.currIndex];
-    if (prevState.currIndex !== this.state.currIndex) {
-      document.getElementById(currComponent.id).focus();
-    }
-  }
-
-  handleKeyDown(event) {
-    let newIndex = this.state.currIndex;
-    if (event.key === 'ArrowDown') newIndex = getNewIndex(ALL_COMPONENTS, this.state.currIndex, 'add');
-    else if (event.key === 'ArrowUp') newIndex = getNewIndex(ALL_COMPONENTS, this.state.currIndex, 'sub');
-    this.setState({ currIndex: newIndex });
-  }
-
   render() {
     return (
       <div id="components-sidebar" className="pam bg-white bas border-gray">
@@ -39,13 +18,11 @@ class Sidebar extends Component {
                     iconName={component.rightIcon.name}
                     iconPosition="left"
                     label={component.label}
-                    onKeyDown={this.handleKeyDown}
                     onClick={() => {
                       this.props.handleStartDrag(component.id)
                     }}
                     variant="base"
                     id={component.id}
-                    tabIndex={i === this.state.currIndex ? "0" : "-1"}
                   />
                   <div>
                     <Icon
