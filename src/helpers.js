6933e3b27fff5aafc98b98a2d736cc54f3c4f320
Lee White <leewhite@salesforce.com>
2019-05-21 15:58:27 -0700

    removed renderCOmponent function

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import classnames from 'classnames';
-import { Button, ButtonGroup } from '@salesforce/design-system-react';
-
 export function cloneObject(obj) {
   return Object.assign({}, obj);
 }
@@ -28,81 +24,4 @@ export function getNewIndex(arr, currIndex, operation) {
 
 export function getObjectbyKey(obj, key, val) {
   return obj.find(el => el[key] === val);
-}
-
-export function renderComponent(componentData, region, handleKeyDown, handleStartDrag, addComponent) {
-  if (typeof (componentData.component) === 'string') {
-    return (
-      <div
-        className={classnames("component",
-          {
-            "grabbed": componentData.isGrabbed,
-          }
-        )}
-        id={componentData.id}
-        key={componentData.id}
-        tabIndex="0"
-        onKeyDown={handleKeyDown}
-        data-type={componentData.value}
-      >
-        <ButtonGroup className="">
-          <Button
-            assistiveText={{ icon: `Grab ${componentData.label}` }}
-            iconCategory="utility"
-            iconName="rows"
-            onClick={handleStartDrag}
-          />
-          <Button
-            assistiveText={{ icon: `Delete ${componentData.label}` }}
-            iconCategory="utility"
-            iconName="delete"
-          />
-        </ButtonGroup>
-        <div
-          className="mbs bg-gray pal"
-        >
-          {componentData.label}
-        </div>
-      </div>
-    );
-  } else {
-    const ReactComponent = componentData.component;
-    return (
-      <div
-        className={classnames("component",
-          {
-            "grabbed": componentData.isGrabbed,
-          }
-        )}
-        data-type={componentData.value}
-        id={componentData.id}
-        key={`component-${componentData.id}`}
-        onKeyDown={handleKeyDown}
-        tabIndex="0"
-      >
-        <ButtonGroup className="">
-          <Button
-            assistiveText={{ icon: `Grab ${componentData.label}` }}
-            iconCategory="utility"
-            iconName="rows"
-            onClick={handleStartDrag}
-          />
-          <Button
-            assistiveText={{ icon: `Delete ${componentData.label}` }}
-            iconCategory="utility"
-            iconName="delete"
-          />
-        </ButtonGroup>
-        <ReactComponent
-          addComponent={addComponent}
-          children={componentData.children}
-          className="mbs"
-          handleKeyDown={handleKeyDown}
-          handleStartDrag={handleStartDrag}
-          id={`${componentData.id}`}
-          region={region}
-        />
-      </div>
-    )
-  }
 }
